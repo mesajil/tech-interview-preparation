6f467f82154a08b16978d96431d36b231a7a8968
Luis Mesajil <lhmesajil@gmail.com>
2023-06-07 09:02:15 -0500

    Add 03 and 06 solutions

--- Ejercicios/03-MaxValue/MaxValue.js
+++ Ejercicios/03-MaxValue/MaxValue.js
@@ -1,6 +1,31 @@
-function MaxValue (shares) {
+function MaxValue(shares) {
   // Your code here:
+  if (!shares.length) return;
+  let min = { index: 0, value: shares[0] };
+  let max = { index: 0, value: shares[0] };
+  let profits = [max.value - min.value]
 
+  for (let i = 1; i < shares.length; i++) {
+    if (shares[i] < min.value){
+      min = { index: i, value: shares[i] }
+      max = { index: i, value: shares[i] }
+    }
+
+    if (shares[i] > max.value)
+      max = { index: i, value: shares[i] }
+
+    if (max.index > min.index)
+      profits.push(max.value - min.value)
+  }
+
+
+  // Return max profit
+  let maxProfit = 0;
+  for (const profit of profits)
+    if (profit > maxProfit)
+      maxProfit = profit;
+  return maxProfit;
 }
+
 
 module.exports = MaxValue
--- Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
+++ Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,22 @@
-function HasBalancedBrackets (string) {
+function HasBalancedBrackets(string) {
   // Your code here:
+  const CHAR = {
+    "{": "}",
+    "(": ")",
+    "[": "]",
+  }
+  const queue = []
+  for (const char of string) {
 
+    const lastChar = queue[queue.length - 1]
+    if (!lastChar || CHAR[lastChar] !== char)
+      queue.push(char)
+    else
+      queue.pop(char)
+  }
+
+  return !queue.length
 }
+
 
 module.exports = HasBalancedBrackets
