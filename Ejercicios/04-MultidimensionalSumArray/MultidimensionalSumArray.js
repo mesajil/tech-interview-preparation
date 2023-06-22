b2a49a17b7b2a04d225ab99e52eb297a81ceed6b
Luis Mesajil <lhmesajil@gmail.com>
2023-06-21 20:25:07 -0500

    Add 04

--- Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,6 +1,19 @@
-function mdArraySum (arr) {
-  // Your code here:
+// function mdArraySum (arr) {
+//   // Your code here:
+//   const first = arr[0]
+//   if (!first) return 0
+//   if (!Array.isArray(first))
+//     return first + mdArraySum(arr.slice(1))
+//   return  mdArraySum(first) + mdArraySum(arr.slice(1))
+// }
 
+
+function mdArraySum(arr) {
+  // Your code here:
+  const first = arr[0]
+  if (!first) return 0
+  const mdArraySumFirst = !Array.isArray(first) ? first : mdArraySum(first)
+  return mdArraySumFirst + mdArraySum(arr.slice(1))
 }
 
 module.exports = mdArraySum
